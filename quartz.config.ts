7a99c95262512e9967e314e8ed0443209f61fd2f
Wesley <boquanchai@gmail.com>
2024-06-15 11:53:14 +0800

    Surface vault backup: 2024-06-15 11:53:14

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Dacnote🖋",
+    pageTitle: "Dacnote",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
@@ -33,7 +33,7 @@ const config: QuartzConfig = {
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
-          secondary: "#284b63",
+          secondary: "#dd2c38",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
@@ -43,7 +43,7 @@ const config: QuartzConfig = {
           gray: "#646464",
           darkgray: "#d4d4d4",
           dark: "#ebebec",
-          secondary: "#7b97aa",
+          secondary: "#ff7881",
           tertiary: "#84a59d",
           highlight: "rgba(143, 159, 169, 0.15)",
         },
